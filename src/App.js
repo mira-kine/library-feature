8973197940a6cebfba6b2ef68ec186b5aea65b66
mira-kine <mirak0419@gmail.com>
2021-12-15 17:13:22 -0800

    changed li elements in booklist to links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import BookDetail from './views/Books/BookDetail';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <BrowserRouter>
+          <header>
+            {/* <NavLink to="/main" data-testid="main-link">
+              Home
+            </NavLink> */}
+            <NavLink to="/books" data-testid="books-link">
+              Books
+            </NavLink>
+          </header>
+          <Switch>
+            <Route path="/main"></Route>
+            <Route path="/books">
+              <BookList />
+            </Route>
+            <Route path="/books:id">
+              <BookDetail />
+            </Route>
+            <Route exact path="/"></Route>
+          </Switch>
+        </BrowserRouter>
+      </main>
+    </>
   );
 }
 
